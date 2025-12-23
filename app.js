d5cb95cc8da680689615a3cd70e7c7c115b6a4c4
Nethi sarath Chandra <nethisarath.chandra@hcl.com>
2025-12-23 17:56:33 +0530

    added comment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require('node:http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Create an HTTP server
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
