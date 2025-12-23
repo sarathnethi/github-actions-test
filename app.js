eed3a7bfa183b3d56b5511e0b3b1682e70ecf1fb
Nethi sarath Chandra <nethisarath.chandra@hcl.com>
2025-12-23 17:50:01 +0530

    added changes to server

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the Home Page\n');
+  } else if (req.url === '/api/data') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: "Hello from 2025!", status: "success" }));
+  } else {
+    res.writeHead(404);
+    res.end('Not Found\n');
+  }
 });
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
